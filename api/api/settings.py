43dd51d7cb51d9ce52572690fcf1206711031364
GiovanniCafasso <giovanni.cafasso01@gmail.com>
2021-12-18 12:57:57 +0100

    added customer-ms.yaml

--- api/api/settings.py
+++ api/api/settings.py
@@ -1,8 +1,6 @@
 from pathlib import Path
 import os
 from dotenv import load_dotenv
-# import json
-# import logging.config
 
 # to manage environment variables
 dotenv_path = Path('api/book-secrets.env')
@@ -10,10 +8,6 @@ load_dotenv(dotenv_path=dotenv_path)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
