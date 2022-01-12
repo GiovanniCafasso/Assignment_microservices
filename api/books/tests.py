df78db21261c1a9c7639cf24edfc094b3e8536e6
GiovanniCafasso <giovanni.cafasso01@gmail.com>
2022-01-12 12:25:18 +0100

    updated book unitest

--- api/books/tests.py
+++ api/books/tests.py
@@ -1,14 +1,33 @@
 from django.test import TestCase    # create a fake db
 from books.models import Book
 from books.serializers import BookSerializer
+from django.test import Client
+# csrf_client = Client(enforce_csrf_checks=True)
 
 class BookTestCase(TestCase):
-    # post 2 books into fake db and get all data
+    # create 2 books and get all
     def test_post_plus_get(self):
         Book.objects.create(title="title1", author="author1")
         Book.objects.create(title="title2", author="author2")
-        print("Created book")
+        print("Created books")
         books = Book.objects.all()
         serializer = BookSerializer(books, many=True)
         print("Serializer book: " + str(serializer.data))
 
+    def test_update(self):
+        Book.objects.create(title="title1", author="author1")
+        print("Created book")
+        Book.objects.update(title="title1", author="Mario")
+        print("Updated book")
+        books = Book.objects.all()
+        serializer = BookSerializer(books, many=True)
+        print("Serializer book: " + str(serializer.data))
+
+    def test_get(self):
+        Book.objects.create(title="title1", author="author1")
+        c = Client()
+        print("All books:")
+        print(c.get('/books/'))
+        print("Book 1:")
+        print(c.get('/books/1'))
+        # c.get('/books/details/', {'name': 'fred', 'age': 7})
