51e6fca2aa6c2b87232a19fcb9a6329481139f6b
GiovanniCafasso <giovanni.cafasso01@gmail.com>
2022-01-19 12:23:18 +0100

    updated test

--- api/books/tests.py
+++ api/books/tests.py
@@ -1,67 +1,59 @@
-# from django.test import TestCase    
+from django.test import TestCase    # create a fake db
 from books.models import Book
 from books.serializers import BookSerializer
-from django.test import Client
-from unittest import TestCase       # create a fake db
+from django.test import Client      
 
-from django.test.client import RequestFactory
+from rest_framework.test import APIRequestFactory
 from books.views import book_detail
 
 class BookTestCase(TestCase):
 
     def setUp(self):
     # Every test needs access to the request factory.
-        self.factory = RequestFactory()
+        self.factory = APIRequestFactory()
+        Book.objects.create(id=1, title="title1", author="author1")
+        Book.objects.create(id=2, title="title2", author="author2")
+        Book.objects.create(id=5, title="title5", author="author5")
+        Book.objects.create(id=10, title="title10", author="author10")
 
-    def test_details(self):
-        print("Test details")
+    def test_details_get(self):
+        print("Test details get")
         # Create an instance of a GET request.
-        request = self.factory.get('/books/1')
+        request = self.factory.get('/books/5')
 
-        # Test views.book_detail() as if it were deployed at /books
-        Book.objects.create(id=10, title="views.book_detail()", author="views.book_detail()")        
-        response = book_detail(request, 10)
+        # Test views.book_detail() as if it were deployed at /books        
+        response = book_detail(request, 5)
         self.assertEqual(response.status_code, 200)
         print("*********************************************")
 
-    # create 2 books and get all
-    def test_post_plus_get(self):
-        print("Test create 2 books and get all")
-        Book.objects.create(title="title1", author="author1")
-        Book.objects.create(title="title2", author="author2")
-        print("Created books")
-        books = Book.objects.all()
-        serializer = BookSerializer(books, many=True)
-        print("Serializer book: " + str(serializer.data))
-        print("*********************************************")
+    def test_details_update(self):
+        print("Test details update")
+        # Create an instance of a PUT request.
+        data = {'id': 1, 'author': 'Mario'}
+        request = self.factory.put('/books/1', data)
 
-    # Create book and update it
-    def test_update(self):
-        print("Test create book and update it")
-        Book.objects.create(title="title1", author="author1")
-        print("Created book")
-        books = Book.objects.all()
-        serializer = BookSerializer(books, many=True)
-        print("Serializer book: " + str(serializer.data))
-        Book.objects.update(title="title1", author="Mario")
-        print("Updated book")
-        books = Book.objects.all()
-        serializer = BookSerializer(books, many=True)
-        print("Serializer book: " + str(serializer.data))
-        print("*********************************************")
-
-    # test get 
-    def test_get(self):
-        print("Test get")
-        Book.objects.create(title="titleA", author="authorA")
-        Book.objects.create(title="titleB", author="authorB")
+        # Test views.book_detail() as if it were deployed at /books        
+        response = book_detail(request, 1)
+        self.assertEqual(response.status_code, 200)
+        print(response)
+        # verify update
         c = Client()
         print("All books:")
         response = c.get('/books/')
         print(response.status_code)
         print(response.content)
-        print("Book 1:")
-        response = c.get('/books/1')
+        print("*********************************************")
+
+    # test get 
+    def test_get(self):
+        print("Test get")
+        c = Client()
+        print("All books:")
+        response = c.get('/books/')
+        print(response.status_code)
+        print(response.content)
+        print("Book 2:")
+        response = c.get('/books/2')
         print(response.status_code)
         print(response.content)
         print("*********************************************")
