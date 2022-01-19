4f7402f6b5f1abd582fb10f09b2ea514d0b08bed
GiovanniCafasso <giovanni.cafasso01@gmail.com>
2022-01-19 13:01:16 +0100

    updated

--- api/books/tests.py
+++ api/books/tests.py
@@ -1,18 +1,18 @@
 from django.test import TestCase    # create a fake db
 from books.models import Book
-from books.serializers import BookSerializer
 from django.test import Client      
 
 from rest_framework.test import APIRequestFactory
 from books.views import book_detail
+from books.views import book_list
 
 class BookTestCase(TestCase):
 
     def setUp(self):
     # Every test needs access to the request factory.
         self.factory = APIRequestFactory()
-        Book.objects.create(id=1, title="title1", author="author1")
-        Book.objects.create(id=2, title="title2", author="author2")
+        Book.objects.create(id=2, title="title1", author="author1")
+        Book.objects.create(id=3, title="title2", author="author2")
         Book.objects.create(id=5, title="title5", author="author5")
         Book.objects.create(id=10, title="title10", author="author10")
 
@@ -29,14 +29,35 @@ class BookTestCase(TestCase):
     def test_details_update(self):
         print("Test details update")
         # Create an instance of a PUT request.
-        data = {'id': 1, 'author': 'Mario'}
-        request = self.factory.put('/books/1', data)
+        data = {'id': 2, 'author': 'Mario'}
+        request = self.factory.put('/books/2', data)
 
         # Test views.book_detail() as if it were deployed at /books        
-        response = book_detail(request, 1)
+        response = book_detail(request, )
         self.assertEqual(response.status_code, 200)
         print(response)
         # verify update
+        c = Client()
+        print("All books:")
+        response = c.get('/books/')
+        print(response.status_code)
+        print(response.content)
+        print("*********************************************")
+
+    def test_post(self):
+        print("Test post")
+        # Create an instance of a POST request.
+        data = {
+        "title": "titoloProva",
+        "author": "authorProva"
+        }
+        request = self.factory.post('/books/', data)
+
+        # Test views.book_detail() as if it were deployed at /books        
+        response = book_list(request)
+        self.assertEqual(response.status_code, 201)
+        print(response)
+        # verify post
         c = Client()
         print("All books:")
         response = c.get('/books/')
